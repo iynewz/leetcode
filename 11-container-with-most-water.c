d49f68fea9ea91379227ecc8947b7590c3f3746e
iynewz <wyzhang00@gmail.com>
2021-08-14 21:48:55 +0800

    water container

---
+++ 11-container-with-most-water.c
@@ -0,0 +1,15 @@
+int maxArea(int* height, int heightSize){
+    int water = 0;
+    int *l = height;
+    int *r = l + heightSize - 1;
+    while (l < r) {
+        int h = *l < *r ? *l : *r;
+        int w = (r - l) * h;
+        if (w > water) {
+            water = w;
+        }
+        while (*l <= h && l < r) l++;
+        while (*r <= h && l < r) r--;
+    }
+    return water;
+}
